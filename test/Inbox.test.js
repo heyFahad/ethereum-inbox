151671566d218950ecd8e7130d86d272e34f9a8f
Fahad Javed <Fahad_Javed008@yahoo.com>
2023-01-25 20:31:55 +0500

    Updated the whole project to use the latest stable versions of all the libraries being used in the project including our local test blockchain network provided by Ganache (instead of ganache-cli)

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const ganache = require('ganache-cli');
+const ganache = require('ganache');
 const Web3 = require('web3');
 const { abi, evm } = require('../compile');
 
